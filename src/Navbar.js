6fe4f470516f19c1e57223a1ecf39f444a057ac1
Janet Meng <jm914@duke.edu>
2024-09-26 22:19:10 -0400

    Pre-styling.

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Janet's Website</h1>
+            <div className="links">
+                <a href = "/"> Home </a>
+                <a href = "/create">My Projects</a>
+            </div>
+        </nav>
+
+     );
+}
+ 
+export default Navbar;
