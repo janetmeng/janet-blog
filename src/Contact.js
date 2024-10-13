1d2a6f60ccca891c7e55c0489ca28e7b5436da90
Janet Meng <jm914@duke.edu>
2024-10-13 17:12:13 -0400

    Styled Contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Contact.css';
 
-const Home = () => {
+const Contact = () => {
     const linkedinlink = "https://www.linkedin.com/in/janet-meng/";
     const gmaillink = "mailto:janet.meng@duke.edu";
     const githublink = "https://github.com/janetmeng";
 
     return (
-        <div className="homepage-content">            
+        <div className="contact">            
          
             {/* LinkedIn Link */}
             <a href={linkedinlink} target="_blank" rel="noopener noreferrer" style={{ color: '#007bff', textDecoration: 'none' }}>
@@ -25,31 +25,4 @@ const Home = () => {
     );
 }
 
-export default Home;
-
-// import React from 'react';
-// import './Contact.css';  // Import the CSS file
-
-// const Contact = () => {
-//   // Define your link variables
-//   const linkedinlink = "https://www.linkedin.com/in/janet-meng/";
-//   const gmaillink = "mailto:janet.meng@duke.edu";
-//   const githublink = "https://github.com/janetmeng";
-
-//   return (
-//     <div className="contact">
-//       <h1>Contact Me</h1>
-//       <a href={linkedinlink} target="_blank" rel="noopener noreferrer" className="contact-link">
-//         Connect with me on LinkedIn
-//       </a>
-//       <a href={gmaillink} target="_blank" rel="noopener noreferrer" className="contact-link">
-//         Email Me
-//       </a>
-//       <a href={githublink} target="_blank" rel="noopener noreferrer" className="contact-link">
-//         Check out my GitHub
-//       </a>
-//     </div>
-//   );
-// }
-
-// export default Contact;
+export default Contact;
